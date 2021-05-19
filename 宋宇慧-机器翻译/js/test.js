d0ff96e565f6f56372d1c9dd6a06a69237808e8a
Leewee-Song <84317091+Leewee-Song@users.noreply.github.com>
2021-05-19 10:17:48 +0800

    Delete 宋宇慧-机器翻译/js directory

--- 宋宇慧-机器翻译/js/test.js
+++
@@ -1,8 +0,0 @@
-addEventListener('load', test)
-
-function test() {
-    var main_wrapper = document.getElementById('main-wrapper');
-    main_wrapper.addEventListener('click', () => {
-        console.log('hello world')
-    })
-}
